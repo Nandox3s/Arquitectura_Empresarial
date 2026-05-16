350d65926c947214812192763306f3c05f2c5b77
Nandox3s <152243013+Nandox3s@users.noreply.github.com>
2026-05-16 11:45:07 -0500

    Taller 1

---
+++ Taller_1.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    // siempre debe tipara las variables o constantes
+
+    val kotlin: String = "jxherrera"
+
+    var variableMutable: Int = 5
+
+    println(kotlin)
+    println(variableMutable)
+
+    variableMutable = 10
+    print(variableMutable)
+
+    val mensaje = "perreo"
+    println(mensaje)
+}
